9b49fad38dc806f5186a9a45587938c938f292f3
raissafadhilah <118705915+raissafadhilah@users.noreply.github.com>
2022-11-29 14:24:21 +0700

    Add files via upload

---
+++ raissa.js
@@ -0,0 +1,5 @@
+var inputnama1 = require('readline-sync');
+var inputnama2 = require('readline-sync');
+var panjang =inputnama1.question('panjang');
+var lebar =inputnama2.question('lebar');
+console.log('luas persegi panjang',panjang * lebar);
